2be37e1aeb37695e210e3876501d4957fc7f8d28
martafreri2018 <73704811+martafreri2018@users.noreply.github.com>
2020-12-27 16:20:24 +0100

    Implemented 3 hour forecast

--- src/index.js
+++ src/index.js
@@ -42,6 +42,7 @@ function change() {
 }
 change();
 
+
 // Bonus challenge
 function changeToFahrenheit(event) {
   event.preventDefault();
@@ -86,10 +87,39 @@ function displayWeatherCondition (response) {
 }
 
 
+function displayForecast (response){
+let forecastElement= document.querySelector("#forecast");
+forecastElement.innerHTML=null;
+let forecast= null;
+
+for (let index=0; index<6; index ++ ) {
+  let forecast= response.data.list[index];
+  forecastElement.innerHTML += `<div class="col-2">
+    <h3>
+     12:00
+    </h3> 
+   
+   <img src="http://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png" alt="" />
+  
+   <div class="weather-forecast-temperature">
+     <strong> ${Math.round(forecast.main.temp_max)}°</strong> | ${Math.round(forecast.main.temp_min)}° 
+   </div>
+   </div>
+   `
+   ;}
+
+}
+
+
+
+
 function searchCity (city) {
   let apiKey= "329ed4c9b33de1b60e6d184aac65a30a"; 
   let apiUrl= `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayWeatherCondition);
+
+  apiUrl= `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`; 
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function handleSubmit (event) {
@@ -115,4 +145,5 @@ form.addEventListener("submit", handleSubmit);
 
 let currentLocationBtn = document.querySelector("#current");
 currentLocationBtn.addEventListener("click", getCurrentLocation);
+
 searchCity("Rome");
